5c3c0770c79ce0cb53324fa55c5646aa3c02399d
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-08-20 15:29:13 +0530

    Update Day 05.java

--- Day 05.java
+++ Day 05.java
@@ -1,18 +1,31 @@
-
-	import java.util.*;
-	//a very compact and simple code which is easy to understand...
-	public class Day5{
-	    public static void main(String[] args){
-	        //object creation for memory allocation to variables...
-	        Scanner sc=new Scanner(System.in);
-	        int n=sc.nextInt();
-	        int a[]=new int[n];
-	        for(int i=0;i<n;i++) a[i]=sc.nextInt();
-	        //reversing the direction of loop
-	        //reverses the way in which array gets printed...
-	        for(int i=n-1;i>=0;i--)
-	        	System.out.print(a[i]+" ");
-	    }
-	}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        bufferedReader.close();
+        Collections.reverse(arr);
+        for(int nums : arr){
+            System.out.print(nums+" ");
+        }
+    }
+}
