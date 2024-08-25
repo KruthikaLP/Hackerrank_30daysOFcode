f5c62ff267ae8ffcc9bf437b0d43c5e80091189a
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-08-25 12:22:55 +0530

    Update and rename Day 09.java to Binary_Numbers.java

---
+++ Binary_Numbers.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        int count=0,max=0;
+        while(n>0){
+            if(n%2==0){
+                count=0;
+            }
+            else{
+                count++;
+                max=Math.max(max,count);
+            }
+           // Move to right bit
+        n=n/2;
+        }
+                
+        System.out.println(max);
+            
+    }
+}
