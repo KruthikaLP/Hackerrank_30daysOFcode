f8dd09d2c968a2594ee7b566aac6963d51a47978
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-09-04 17:08:39 +0530

    Update More Exceptions.java

--- More Exceptions.java
+++ More Exceptions.java
@@ -4,7 +4,7 @@ import java.io.*;
 //Write your code here
 class Calculator {
     int power(int n,int p)throws Exception{
-        if(n<=0 || p<=0){
+        if(n<0 || p<0){
            throw new Exception("n and p should be non-negative");
         }
             return (int)Math.pow(n, p);
