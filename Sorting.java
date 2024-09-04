7ceb4d4abff2763c2d1d13327c479121b3275c58
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-09-04 21:26:08 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,34 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-	import java.util.*;
-	public class Day11 {
-	    //bubble sort is mentioned in question..
-	    //else the best sorting algorithm would be quick sort...
-	    public static void main(String[] args) {
-	        Scanner sc=new Scanner(System.in);
-	        int n = sc.nextInt();
-	        int[] a=new int[n];
-	        for(int i=0;i<n;i++)
-	            a[i]=sc.nextInt();
-	        int numberOfSwaps=0;
-	        for (int i=0;i<n;i++) 
-	        {
-	            for (int j=0;j<n-1;j++)
-	            {
-	                if (a[j]>a[j+1])
-	                {
-	                    //below three lines are used for swapping...
-	                    int temp=a[j];
-	                    a[j]=a[j+1];
-	                    a[j+1]=temp;
-	                    numberOfSwaps++;
-	                }
-	            }
-	            if (numberOfSwaps==0) break;
-	        }
-	        System.out.println("Array is sorted in "+numberOfSwaps+" swaps.");
-	        System.out.println("First Element: "+a[0]);
-	        System.out.println("Last Element: "+a[n-1]);
-	    }
-	}
 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        // Write your code here
+         int numberOfSwaps = 0;
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a.size() - i - 1; j++) {  
+                    int temp = a.get(j);
+                    a.set(j, a.get(j + 1));
+                    a.set(j + 1, temp);
+                    
+                    numberOfSwaps++;
+                }
+            }
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(a.size() - 1));
+    }
 }
